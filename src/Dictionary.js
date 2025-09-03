54d2f8034a88e5f470d94a0e2565429f4f848554
Iryna Khandoniak <irr.enn.booking@gmail.com>
2025-09-03 18:22:26 +0100

    fixed an issue

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,31 @@
-import React from "react"
+import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
+
 export default function Dictionary() {
-    return 
+  let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
     <div className="Dictionary">
-    Hello from Dictionary</div>
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
 }
