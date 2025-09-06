a157961b1082d2b3d610d73d0067cfc14a368a31
Iryna Khandoniak <irr.enn.booking@gmail.com>
2025-09-06 19:41:31 +0100

    ddd

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,22 +1,14 @@
 import React from "react";
+import "./Phonetic.css";
 
-export default function Phonetic({ phonetic }) {
-  const audio = phonetic?.audio;
-  const text = phonetic?.text;
-
-  if (!audio && !text) return null;
-
+export default function Phonetic(props) {
+  console.log(props.phonetic);
   return (
     <div className="Phonetic">
-      {audio && (
-        <>
-          <a href={audio} target="_blank" rel="noopener noreferrer">
-            Listen
-          </a>
-          <br />
-        </>
-      )}
-      {text && <span>{text}</span>}
+      <a href={props.phonetic.audio} target="_blank">
+        Listen
+      </a>
+      <span className="text">{props.phonetic.text}</span>
     </div>
   );
 }
